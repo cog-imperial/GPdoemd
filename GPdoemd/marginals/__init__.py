d6dc35800823ed898499df45a24d854ac1eb6e6b
Simon-Olofsson <simonolofsson91@gmail.com>
2018-04-06 14:31:35 +0200

    Updated __init__ file for new marginal

--- GPdoemd/marginals/__init__.py
+++ GPdoemd/marginals/__init__.py
@@ -1,5 +1,7 @@
 
-from .marginal import Marginal
+from .gpmarginal import GPMarginal
 from .taylor_first import TaylorFirstOrder
 from .taylor_second import TaylorSecondOrder
+from .analytic import Analytic
+from .numerical import Numerical
 
