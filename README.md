155b2f3ab19e635012b1d82f2c96babb10df506a
Simon-Olofsson <simonolofsson91@gmail.com>
2018-04-18 09:05:02 +0100

    Testing Travis

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # GPdoemd
+[![Build Status](https://travis-ci.org/scwolof/GPdoemd.svg?branch=dev)](https://travis-ci.org/scwolof/GPdoemd)
+
 Design of experiments for model discrimination using Gaussian process surrogate models
 
-Package requires numpy, scipy and GPy to work.
+Package requires:
+* numpy
+* scipy 
+* GPy
 
--- tests/test_model.py
+++ tests/test_model.py
@@ -1,73 +1,92 @@
 
-import unittest
+import pytest
 import numpy as np 
-from GPdoemd.models import Model
+from GPdoemd.models import GPModel
 
-class TestClass (unittest.TestCase):
+from pdb import set_trace as st
 
-	def __init__ (self):
-		self.x_bounds = np.array([[10., 20.], [5., 8.]])
-		self.p_bounds = np.array([[ 2.,  4.], [3., 5.]])
+"""
+SET UP MODEL ONCE
+"""
 
-		X  = np.array([[10., 5.], [10., 8.], [20., 5.], [20., 8.]])
-		Xs = np.random.uniform([10, 5], [20, 8], size=(10,2))
-		self.X = np.vstack(( X, Xs ))
+x_bounds = np.array([[10., 20.], [5., 8.]])
+p_bounds = np.array([[ 2.,  4.], [3., 5.]])
 
-		P  = np.array([[2., 3.], [2., 5.], [4., 3.], [4., 5.]])
-		Ps = np.random.uniform([2., 3.], [4., 5.], size=(10,2))
-		self.P = np.vstack(( P, Ps ))
+def f (x, p):
+	return x * p
 
-		self.Z = np.c[self.X, self.P]
+d = {
+	'name':        'testmodel',
+	'call':        f,
+	'x_bounds':    x_bounds,
+	'p_bounds':    p_bounds,
+	'num_outputs': 2
+}
+M = GPModel(d)
 
-		def f (x, p):
-			return x * p
-		
-		self.p = np.array([3., 4.])
-		self.Y = f(self.X, self.p)
 
-		self.d = {
-			'name':        'testmodel',
-			'call':        f,
-			'x_bounds':    self.x_bounds,
-			'p_bounds':    self.p_bounds,
-			'num_outputs': self.Y.shape[1]
-		}
-		self.M = Model(self.d)
+X  = np.array([[10., 5.], [10., 8.], [20., 5.], [20., 8.]])
+Xs = np.random.uniform([10, 5], [20, 8], size=(10,2))
+X = np.vstack(( X, Xs ))
 
-		self.M.set_training_data(self.Z, self.Y)
+P  = np.array([[2., 3.], [2., 5.], [4., 3.], [4., 5.]])
+Ps = np.random.uniform([2., 3.], [4., 5.], size=(10,2))
+P = np.vstack(( P, Ps ))
+
+Z = np.c_[X, P]
+Y = f(X, P)
+M.set_training_data(Z, Y)
+
+
+"""
+TESTS
+"""
+
+class TestGPModel:
 
 	def test_call(self):
-		F = self.M.call(self.X, self.p)
-		self.assertEqual(F, self.Y)
+		p = np.array([3., 4.])
+		F = M.call(X, p)
+		assert np.all(F == f(X, p))
 
-	def test_zmin (self):
-		zmin = np.array([-1,-1])
-		self.assertEqual(self.M.zmin, zmin)
+	def test_trans_z_min (self):
+		zmin = np.array([-1,-1,-1,-1])
+		assert np.all( np.min(M.Z, axis=0) == zmin )
 
-	def test_zmax (self):
-		zmax = np.array([ 1, 1])
-		self.assertEqual(self.M.zmax, zmax)
+	def test_trans_z_max (self):
+		zmax = np.array([ 1, 1, 1, 1])
+		assert np.all( np.max(M.Z, axis=0) == zmax )
 
-	def test_ymin (self):
+	def test_trans_x_min (self):
+		xmin = np.array([-1,-1])
+		assert np.all( np.min(M.X, axis=0) == xmin )
+
+	def test_trans_x_max (self):
+		xmax = np.array([ 1, 1])
+		assert np.all( np.max(M.X, axis=0) == xmax )
+
+	def test_trans_p_min (self):
+		pmin = np.array([-1,-1])
+		assert np.all( np.min(M.P, axis=0) == pmin )
+
+	def test_trans_p_max (self):
+		pmax = np.array([ 1, 1])
+		assert np.all( np.max(M.P, axis=0) == pmax )
+
+	def test_trans_y_min (self):
 		ymin = np.array([-1,-1])
-		self.assertEqual(self.M.ymin, ymin)
+		assert np.all( np.min(M.Y, axis=0) == ymin )
 
-	def test_ymax (self):
+	def test_trans_y_max (self):
 		ymax = np.array([ 1, 1])
-		self.assertEqual(self.M.ymax, ymax)
+		assert np.all( np.max(M.Y, axis=0) == ymax )
 
-	def backtrans_y_min (self):
+	def test_backtrans_y_min (self):
 		ymin = np.array([20,15])
-		yt   = self.M.backtransform_y(np.array([-1,-1]))
-		self.assertEqual(yt, ymin)
+		yt   = M.backtransform_y( np.array([-1,-1]) )
+		assert np.all(yt == ymin)
 
-	def backtrans_y_max (self):
+	def test_backtrans_y_max (self):
 		ymax = np.array([80,40])
-		yt   = self.M.backtransform_y(np.array([ 1, 1]))
-		self.assertEqual(yt, ymax)
-
-if __name__ == '__main__':
-	unittest.main()
-
-
-
+		yt   = M.backtransform_y( np.array([ 1, 1]) )
+		assert np.all(yt == ymax)
