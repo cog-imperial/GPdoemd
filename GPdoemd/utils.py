a603e1990b296b038750f89f6326c8091a4e31f3
Simon-Olofsson <simonolofsson91@gmail.com>
2018-04-18 17:23:37 +0100

    Removed expand_dims

--- GPdoemd/utils.py
+++ GPdoemd/utils.py
@@ -1,23 +1,19 @@
 
 import numpy as np
-from scipy.stats import multivariate_normal as mv
 
-
+"""
 def expand_dims (a,axis):
-	"""
-	Expand dims where axis can be a list
-	"""
+	## Expand dims where axis can be a list
 	if isinstance(axis,int):
 		return np.expand_dims(a,axis)
 	b = a.copy()
 	for ax in axis:
 		b = np.expand_dims(b,ax)
 	return b
-
+"""
 
 def binary_dimensions (Z, binary_variables):
 	"""
-	Return 
 
 	Inputs
 		Z 					Array of design variables (+ parameter (optional))
@@ -48,25 +44,4 @@ def binary_dimensions (Z, binary_variables):
 				J.append(j)
 				break
 	return range(lb), np.array(I), np.array(J)
-
-
-#def marginal_predict (X, Ms):
-	"""
-	Return means and covariances for predictions from all models
-
-	inputs
-		X   [ N x d ]	Array of designs
-		Ms  [ M ] 		List of models
-
-	Outputs
-		mu 	[ N x M x E]		Prediction means
-		s2  [ N x M x E x E ]	Prediction covariances
-	"""
-	"""
-	N, M, E = len(X), len(Ms), 1
-	mu, s2  = np.zeros((N,M,E)), np.zeros((N,M,E,E))
-	for i, M in enumerate(Ms):
-		mu[:,i], s2[:,i] = M.marginal_predict(X)
-	return mu, s2
-	"""
 
