f63304ba09a5d24a6c6509f8e7ac9dbb0064a978
scwolof <scw.olofsson@gmail.com>
2018-05-01 11:00:45 +0100

    Updated stationary kernel tests

--- tests/test_kernels/test_stationary.py
+++ tests/test_kernels/test_stationary.py
@@ -27,6 +27,18 @@ class TestStationaryKernels:
 	def _get_gp (self, kern):
 		return GPRegression(X, Y, kern(d, dr, 'name'))
 
+	def _dr2 (self, gp):
+		return gp.kern.dK2_drdr(R), R.shape
+
+	def _dX (self, gp):
+		xx = gp._predictive_variable
+		return gp.kern.gradients_X(1,X[:5],xx), (5, X.shape[1])
+
+	def _dXX (self, gp):
+		xx = gp._predictive_variable
+		return gp.kern.gradients_XX(1,X[:5],xx), (5, N, X.shape[1], X.shape[1])
+
+	"""
 	def test_stationary(self):
 		k   = GPdoemd.kernels.RBF
 		gp  = self._get_gp(k)
@@ -46,39 +58,70 @@ class TestStationaryKernels:
 		
 		dx = gp.kern.d2_k_d_x2(X1,X2)
 		assert dx.shape == (n,m,d,d)
+	"""
 
 	def test_rbf(self):
-		k   = GPdoemd.kernels.RBF
-		gp  = self._get_gp(k)
-		ddK = gp.kern.dK2_drdr(R)
-		assert ddK.shape == R.shape
+		gp  = self._get_gp( GPdoemd.kernels.RBF )
+		D,r = self._dr2( gp )
+		assert D.shape == r
+		D,r = self._dX( gp )
+		assert D.shape == r
+		D,r = self._dXX( gp )
+		assert D.shape == r
+		#ddK = gp.kern.dK2_drdr(R)
+		#assert ddK.shape == R.shape
 
 	def test_exponential(self):
-		k   = GPdoemd.kernels.Exponential
-		gp  = self._get_gp(k)
-		ddK = gp.kern.dK2_drdr(R)
-		assert ddK.shape == R.shape
+		gp  = self._get_gp( GPdoemd.kernels.Exponential )
+		D,r = self._dr2( gp )
+		assert D.shape == r
+		D,r = self._dX( gp )
+		assert D.shape == r
+		D,r = self._dXX( gp )
+		assert D.shape == r
+		#ddK = gp.kern.dK2_drdr(R)
+		#assert ddK.shape == R.shape
 
 	def test_matern32(self):
-		k   = GPdoemd.kernels.Matern32
-		gp  = self._get_gp(k)
-		ddK = gp.kern.dK2_drdr(R)
-		assert ddK.shape == R.shape
+		gp  = self._get_gp( GPdoemd.kernels.Matern32 )
+		D,r = self._dr2( gp )
+		assert D.shape == r
+		D,r = self._dX( gp )
+		assert D.shape == r
+		D,r = self._dXX( gp )
+		assert D.shape == r
+		#ddK = gp.kern.dK2_drdr(R)
+		#assert ddK.shape == R.shape
 
 	def test_matern52(self):
-		k   = GPdoemd.kernels.Matern52
-		gp  = self._get_gp(k)
-		ddK = gp.kern.dK2_drdr(R)
-		assert ddK.shape == R.shape
+		gp  = self._get_gp( GPdoemd.kernels.Matern52 )
+		D,r = self._dr2( gp )
+		assert D.shape == r
+		D,r = self._dX( gp )
+		assert D.shape == r
+		D,r = self._dXX( gp )
+		assert D.shape == r
+		#ddK = gp.kern.dK2_drdr(R)
+		#assert ddK.shape == R.shape
 
 	def test_cosine(self):
-		k   = GPdoemd.kernels.Cosine
-		gp  = self._get_gp(k)
-		ddK = gp.kern.dK2_drdr(R)
-		assert ddK.shape == R.shape
+		gp  = self._get_gp( GPdoemd.kernels.Cosine )
+		D,r = self._dr2( gp )
+		assert D.shape == r
+		D,r = self._dX( gp )
+		assert D.shape == r
+		D,r = self._dXX( gp )
+		assert D.shape == r
+		#ddK = gp.kern.dK2_drdr(R)
+		#assert ddK.shape == R.shape
 
 	def test_ratquad(self):
-		k   = GPdoemd.kernels.RatQuad
-		gp  = self._get_gp(k)
-		ddK = gp.kern.dK2_drdr(R)
-		assert ddK.shape == R.shape
+		gp  = self._get_gp( GPdoemd.kernels.RatQuad )
+		D,r = self._dr2( gp )
+		assert D.shape == r
+		D,r = self._dX( gp )
+		assert D.shape == r
+		D,r = self._dXX( gp )
+		assert D.shape == r
+		#ddK = gp.kern.dK2_drdr(R)
+		#assert ddK.shape == R.shape
