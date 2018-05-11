1c074edc22d402f09bcbbbbcd2891db6fa4b5ac7
scwolof <scw.olofsson@gmail.com>
2018-05-11 11:02:34 +0100

    Added license header

--- GPdoemd/case_studies/analytic/vthr2014linear.py
+++ GPdoemd/case_studies/analytic/vthr2014linear.py
@@ -1,8 +1,25 @@
 """
-Case study from
-J. Vanlier, C. A. Tiemann, P. A. J. Hilbers, and N. A. W. van Riel 
-"Optimal experiment design for model selection in biochemical networks" 
-BMC Systems Biology, 8(20), 2014.
+MIT License
+
+Copyright (c) 2018 Simon Olofsson
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 """
 
 import numpy as np 
@@ -11,6 +28,12 @@ import numpy as np
 Model super class
 """
 class VTHR2014Model:
+	"""
+	Case study from
+	J. Vanlier, C. A. Tiemann, P. A. J. Hilbers, and N. A. W. van Riel 
+	"Optimal experiment design for model selection in biochemical networks" 
+	BMC Systems Biology, 8(20), 2014.
+	"""
 	@property
 	def name (self):
 		return 'M' + str(self.num_p)
@@ -24,7 +47,7 @@ class VTHR2014Model:
 	def p_bounds (self):
 		return np.array([[-10,10]] * self.num_p)
 
-	def __call__ (self,x,P,grad=False):
+	def __call__ (self, x, P, grad=False):
 		F = [	
 			lambda t: t,
 			lambda t: t**2,
