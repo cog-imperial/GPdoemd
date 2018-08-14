bc6020266cb432468253acc86e4e2bd4a83c9b41
scwolof <scw.olofsson@gmail.com>
2018-08-14 12:17:29 +0100

    Import updated models

--- GPdoemd/models/__init__.py
+++ GPdoemd/models/__init__.py
@@ -1,5 +1,8 @@
 
 from .model import Model
 
-from .gp_model import GPModel
+from .analytic_model import AnalyticModel
+
+from .surrogate_model import SurrogateModel
+from .vanilla_gp_model import VanillaGPModel
 from .sparse_gp_model import SparseGPModel
