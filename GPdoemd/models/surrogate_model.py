8c5ad9713d2b4bddd945dac05749f0f3643d2841
scwolof <scw.olofsson@gmail.com>
2018-08-17 15:10:27 +0100

    Removed delete safety check

--- GPdoemd/models/surrogate_model.py
+++ GPdoemd/models/surrogate_model.py
@@ -38,18 +38,6 @@ class SurrogateModel (Model):
 	"""
 	Properties
 	"""
-	# Measurement noise variance transformed to GP y-space
-	"""
-	@property
-	def transformed_meas_noise_var (self):
-		if self.meas_noise_var.ndim == 1:
-			#return self.transform_y_var(self.meas_noise_var)
-			return self.trans_y.var(self.meas_noise_var)
-		else:
-			#return self.transform_y_cov(self.meas_noise_var)
-			return self.trans_y.cov(self.meas_noise_var)
-	"""
-
 	@property
 	def binary_variables (self):
 		return self._binary_variables
@@ -94,32 +82,26 @@ class SurrogateModel (Model):
 			self.trans_z = BoxTransform(self.zmin, self.zmax)
 			self.trans_x = BoxTransform(self.xmin, self.xmax)
 			self.trans_p = BoxTransform(self.pmin, self.pmax)
-			#self._Z     = self.transform_z(value)
 			self._Z      = self.trans_z(value)
 	@Z.deleter
 	def Z (self):
-		self._Z    = None
-		#self._zmin = None
-		#self._zmax = None
-		#del self._Z   
-		del self._zmin
-		del self._zmax
-		del self.trans_z
-		del self.trans_x
-		del self.trans_p
+		if self.Z is not None:
+			del self._zmin
+			del self._zmax
+			del self.trans_z
+			del self.trans_x
+			del self.trans_p
+		self._Z = None
 	@property
 	def zmin (self):
-		#return None if not hasattr(self,'_zmin') else self._zmin
 		return self._zmin
 	@property
 	def zmax (self):
-		#return None if not hasattr(self,'_zmax') else self._zmax
 		return self._zmax
 
 	def get_Z (self, X, P=None):
 		if P is None:
 			assert self.pmean is not None
-			#P = np.asarray([self.transform_p(self.pmean)] * len(X))
 			P = np.asarray([self.trans_p(self.pmean)] * len(X))
 		assert P.shape == ( len(X), self.dim_p )
 		return np.array([ x.tolist() + p.tolist() for x,p in zip(X,P) ])
@@ -128,28 +110,22 @@ class SurrogateModel (Model):
 	@property
 	def X (self):
 		return None if self.Z is None else self.Z[:,:self.dim_x]
-		#return self.Z[:,:self.dim_x]
 	@property
 	def xmin (self):
-		#return None if self.zmin is None else self.zmin[:self.dim_x]
 		return self.zmin[:self.dim_x]
 	@property
 	def xmax (self):
-		#return None if self.zmax is None else self.zmax[:self.dim_x]
 		return self.zmax[:self.dim_x]
 
 	## Training model parameter values
 	@property
 	def P (self):
 		return None if self.Z is None else self.Z[:,self.dim_x:]
-		#return self.Z[:,self.dim_x:]
 	@property
 	def pmin (self):
-		#return None if self.zmin is None else self.zmin[self.dim_x:]
 		return self.zmin[self.dim_x:]
 	@property
 	def pmax (self):
-		#return None if self.zmax is None else self.zmax[self.dim_x:]
 		return self.zmax[self.dim_x:]
 
 	## Training targets
@@ -167,20 +143,16 @@ class SurrogateModel (Model):
 			self._Y      = self.trans_y(value)
 	@Y.deleter
 	def Y (self):
-		self._Y     = None
-		#self._ymean = None
-		#self._ystd  = None
-		#del self._Y    
-		del self._ymean
-		del self._ystd
-		del self.trans_y
+		if self.Y is not None:
+			del self._ymean
+			del self._ystd
+			del self.trans_y
+		self._Y = None
 	@property
 	def ymean (self):
-		#return None if not hasattr(self,'_ymean') else self._ymean
 		return self._ymean
 	@property
 	def ystd (self):
-		#return None if not hasattr(self,'_ystd') else self._ystd
 		return self._ystd
 
 
@@ -191,121 +163,6 @@ class SurrogateModel (Model):
 		else:
 			self.Z = np.c_[Z, Y]
 			self.Y = _Y
-
-
-	"""
-	Variable, parameter and target transformations
-	"""
-	"""
-	def _none_check (self, arglist):
-		if np.any([a is None for a in arglist]):
-			return True
-		return False
-
-	## Transform input to interval [0,1]
-	def box_trans (self, X, xmin, xmax, reverse=False):
-		if reverse:
-			return xmin + X * (xmax - xmin)
-		return (X - xmin) / (xmax - xmin)
-
-	def box_var_trans (self, X, xmin, xmax, reverse=False):
-		m = xmax - xmin
-		if reverse:
-			return X * m**2
-		return X / m**2
-
-	def box_cov_trans (self, X, xmin, xmax, reverse=False):
-		m = xmax - xmin
-		if reverse:
-			return X * (m[:,None] * m[None,:])
-		return X / (m[:,None] * m[None,:])
-
-
-	# Transform such that input has mean zero
-	def mean_trans (self, X, mean, std, reverse=False):
-		if reverse:
-			return mean + X * std
-		return (X - mean) / std
-
-	def mean_var_trans (self, X, mean, std, reverse=False):
-		if reverse:
-			return X * std**2
-		return X / std**2
-
-	def mean_cov_trans (self, X, mean, std, reverse=False):
-		if reverse:
-			return X * (std[:,None] * std[None,:])
-		return X / (std[:,None] * std[None,:])
-
-
-	# Transform to transform-space
-	def transform (self, trans, X, x1, x2, reverse=False):
-		if self._none_check([X, x1, x2]):
-			warnings.warn('Transform value is None')
-			return np.NaN
-		return trans( X, x1, x2, reverse=reverse )
-
-	# Transform back to original space
-	def backtransform (self, trans, X, x1, x2):
-		return self.transform(trans, X, x1, x2, reverse=True)
-	
-	## Different transforms
-	def transform_x (self, X):
-		return self.transform(self.box_trans, X, self.xmin, self.xmax)
-
-	def backtransform_x (self, X):
-		return self.backtransform(self.box_trans, X, self.xmin, self.xmax)
-
-	def transform_p (self, P):
-		return self.transform(self.box_trans, P, self.pmin, self.pmax)
-	
-	def backtransform_p (self, P):
-		return self.backtransform(self.box_trans, P, self.pmin, self.pmax)
-	
-	def transform_z (self, Z):
-		return self.transform(self.box_trans, Z, self.zmin, self.zmax)
-	
-	def backtransform_z (self, Z):
-		return self.backtransform(self.box_trans, Z, self.zmin, self.zmax)
-	
-	def transform_y (self, Y):
-		return self.transform(self.mean_trans, Y, self.ymean, self.ystd)
-	
-	def backtransform_y (self, Y):
-		return self.backtransform(self.mean_trans, Y, self.ymean, self.ystd)
-
-	def transform_p_var (self, C):
-		return self.transform(self.box_var_trans, C, self.pmin, self.pmax)
-	
-	def backtransform_p_var (self, C):
-		return self.backtransform(self.box_var_trans, C, self.pmin, self.pmax)
-	
-	def transform_p_cov (self, C):
-		return self.transform(self.box_cov_trans, C, self.pmin, self.pmax)
-	
-	def backtransform_p_cov (self, C):
-		return self.backtransform(self.box_cov_trans, C, self.pmin, self.pmax)
-	
-	def transform_y_var (self, C):
-		return self.transform(self.mean_var_trans, C, self.ymean, self.ystd)
-	
-	def backtransform_y_var (self, C):
-		return self.backtransform(self.mean_var_trans, C, self.ymean, self.ystd)
-	
-	def transform_y_cov (self, C):
-		return self.transform(self.mean_cov_trans, C, self.ymean, self.ystd)
-	
-	def backtransform_y_cov (self, C):
-		return self.backtransform(self.mean_cov_trans, C, self.ymean, self.ystd)
-
-	def backtransform_prediction (self, M, S):
-		M = self.backtransform_y(M)
-		if S.ndim == 2:
-			S = self.backtransform_y_var(S)
-		else:
-			S = self.backtransform_y_cov(S)
-		return M, S
-	"""
 
 	"""
 	Prediction
