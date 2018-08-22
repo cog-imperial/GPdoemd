f822401b5e5b57e43fbbb2f206fe8c47fcd759dd
scwolof <scw.olofsson@gmail.com>
2018-08-22 16:19:45 +0100

    Bump version

--- GPdoemd/__version__.py
+++ GPdoemd/__version__.py
@@ -1,5 +1,5 @@
 # pragma: no cover
-__version__      = '1.0'
+__version__      = '1.1'
 __author__       = 'Simon Olofsson'
 __author_email__ = 'simon.olofsson15@imperial.ac.uk'
 __license__      = 'MIT License'
